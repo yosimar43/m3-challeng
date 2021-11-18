8ac0060726e4f7f22b3a7f6f0cb9a2cbcbe7a0e9
Yosimar Sinisterra <michaelyosimar43@gmail.com>
2021-11-17 21:55:15 -0500

    Feature(mongodb): create schema to new products

---
+++ src/backend/models/products.ts
@@ -0,0 +1,37 @@
+// Scheme to products
+import { Schema, model, models } from "mongoose";
+
+const productsSchema = new Schema({
+ name: {
+  type: String,
+  required: true,
+  unique: true,
+  trim: true,
+ },
+ price: {
+  type: Number,
+  required: true,
+ },
+ parcelamento: {
+  type: Array,
+  required: true,
+ },
+ color: {
+  type: String,
+  required: true,
+ },
+ image: {
+  type: String,
+  required: true,
+ },
+ size: {
+  type: Array,
+  required: true,
+ },
+ date: {
+  type: String,
+  required: true,
+ },
+});
+
+export default models.Task || model("Products", productsSchema);
